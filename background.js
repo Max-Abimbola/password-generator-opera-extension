b8d3e535b088c6bea8e1786fd7a20a4ea37f6602
Max-Abimbola <M.Abimbola2@newcastle.ac.uk>
2022-11-15 13:08:46 +0000

    chore:rebasing

--- background.js
+++ background.js
@@ -4,9 +4,15 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const symbols = "~`!@#$%^&*()_-+={[}]|\:;'<,>.?/";
 
+var incLetters = false;
+var incNumbers = false;
+var incSymbols = false;
+
 
 function displayPassword(value){
+
 	var password_text_area = document.getElementById("password-text-area");
+	password_text_area.value = ""
 	password_text_area.value = value;
 }
 
@@ -18,24 +24,43 @@ function hello(){
 	console.log("hello");
 }
 
-function generatePassword(password_length){
+function generatePassword(incLetters,incNumbers,incSymbols,password_length){
 
 	var password = "";
+	console.log(`incLetters: ${incLetters} , incNumbers: ${incNumbers} , incSymbols: ${incSymbols}`);
 
-	for(i=0;i<password_length;i++){
-		let set = getRandomInt(2);
-		switch(set){
-			case 0:
+	available_characters = [];
+
+	if(incLetters == true){
+		available_characters.push("incLetters");
+	}
+	if(incNumbers == true){
+		available_characters.push("incNumbers");
+	}
+	if(incSymbols == true){
+		available_characters.push("incSymbols");
+	}
+
+	console.log("available characters: "+available_characters)
+
+	for(i=0;i<password_length-1;i++){
+
+		let set = getRandomInt(available_characters.length-1);
+
+		console.log("available characters array:",available_characters[set]);
+
+		switch(available_characters[set]){
+			case "incLetters":
 				var randnum = getRandomInt(letters.length);
 				password += letters[randnum];
 
-			case 1:
+			case "incNumbers":
 				var randnum = getRandomInt(numbers.length);
 				password += numbers[randnum];
 
-			case 2:
+			case "incSymbols":
 				var randnum = getRandomInt(symbols.length);
-				password += symbols[randnum];
+				password += symbols[randnum]
 		}
 	}
 
